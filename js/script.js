0df722fd8258d772a9f902e0b9437126f42dc3a8
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-02-08 22:20:16 +0100

    Add score table

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
 'use strict';
 
 var scoreOutput = document.getElementById("output");
+var scoreTable = document.getElementById("scoreTable");
+var playerScore = 0;
+var compScore = 0;
 
-// Funkcja dotycząca kto wygra
-var playerPick = function(playerPick) {
-  var comp = compMove();
-
-  if (playerPick === comp) {
-    scoreOutput.innerHTML = "Draw";
-  }
-  else if (playerPick === 'rock' && comp === 'paper' || playerPick === "scissors" && comp === "rock" || playerPick === "rock" && comp === "paper") {
-    scoreOutput.innerHTML = "Wygrywa komputer";
-  }
-  else {
-    scoreOutput.innerHTML = "Wygrywa gracz";
-  }
-};
 
 var playerPickRock = document.getElementById("player1Rock");
 var playerPickScissors = document.getElementById("player1Scissors");
 var playerPickPaper = document.getElementById("player1Paper");
 
-// Deklaracja ruchu gracza
+//////////////////////////////
+//  Funkcja dotycząca kto wygra  //
+//////////////////////////////
+var playerPick = function(playerPick) {
+  var comp = compMove();
+
+  if (playerPick === comp) {
+    scoreOutput.innerHTML = "Draw";
+    scoreTable.innerHTML = ;
+  }
+  else if (playerPick === 'rock' && comp === 'paper' || playerPick === "scissors" && comp === "rock" || playerPick === "rock" && comp === "paper") {
+    compScore++;
+    scoreOutput.innerHTML = "Wygrywa komputer";
+    scoreTable.innerHTML = "Wygrywa komputer "  + playerScore + " Computer:" + compScore;
+  }
+  else {
+    playerScore++;
+    scoreOutput.innerHTML = "Wygrywa gracz";
+    scoreTable.innerHTML = "Wygrywa gracz: "  + playerScore + " Computer: " + compScore;
+  }
+};
+/////////////////////////////////
+// Deklaracja ruchu gracza /////
+/////////////////////////////////
 playerPickRock.addEventListener('click', function() {
   playerPick('rock');
 });
@@ -33,8 +45,9 @@ playerPickScissors.addEventListener('click', function(){
 playerPickPaper.addEventListener('click', function() {
   playerPick('paper');
 });
-
-// Losuje ruch dla komputerar
+///////////////////////////////
+// Losuje ruch dla komputerar ///
+//////////////////////////////
 function compMove() {
   var compPick = ['rock', 'paper','scissors'];
   return compPick[Math.floor(Math.random() * 3)];
